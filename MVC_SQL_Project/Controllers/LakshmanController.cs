dc380182c511a7cb08cb3d78c82f11dd33b88645
Saiteja Ravella <saiteja.ravella@dxc.com>
2020-10-05 14:48:10 +0530

    created

---
+++ MVC_SQL_Project/Controllers/LakshmanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_SQL_Project.Controllers
+{
+    public class LakshmanController : Controller
+    {
+        // GET: Lakshman
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
